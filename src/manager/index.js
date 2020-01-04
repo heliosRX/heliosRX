d04790d7a5c56991437bbac540afcc683139f286
Thomas <tw@goalpilot.com>
2020-01-04 10:58:59 -0500

    :construction: store manager class

---
+++ src/manager/index.js
@@ -0,0 +1,38 @@
+// StoreManager
+
+export default {
+
+  /*
+  *
+  */
+  registerStore() {
+  },
+
+  /*
+  *
+  */
+  removeStore() {
+  },
+
+  /*
+  *
+  */
+  getAllStores() {
+
+  },
+
+  /*
+  *
+  */
+  resetAllStores() {
+  },
+
+  // TODO: Use for generator too
+
+  // getGlobalSubscriptionList
+  // setDefaultDB
+  // getRegistryState
+  // getAllSyncedPaths
+  // reset_global_instance_cache
+  // get subscriptions()
+}

60ce24464d42e3b2937e9b5ac2ad7fcb9a205260
Thomas <tw@goalpilot.com>
2020-01-11 16:00:06 -0500

    :art: update example / new options syntax

--- examples/dev-env/src/main.js
+++ examples/dev-env/src/main.js
@@ -3,12 +3,9 @@ import VueRouter from 'vue-router'
 import ElementUI from 'element-ui';
 
 import heliosRX from 'heliosrx'
-import GenericStore from 'heliosrx/src/store'
 import App from './App.vue'
 
 import { rtdb } from './firebase'
-import db from "heliosrx/src/global_api"; // TODO
-import api from 'heliosrx/src/api'
 import models from "@/models" // TODO
 
 import router from '@/router'
@@ -18,21 +15,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 Vue.use(ElementUI);
 Vue.use(heliosRX, {
+  devMode: true,
   models: models,
-  api: api,
-  // TODO: passs rtdb here
+  userApi: {},
+  db: rtdb,
 })
-
-GenericStore.setDefaultDB( rtdb );
-
-// import { get_registry } from 'heliosrx/api/misc'
-api.get_registry().commit('INIT_REGISTRY');
-
-// TODO: MOVE
-if ( process.env.VUE_APP_PRODUCTION === 'false' ) {
-  window.$db = db;
-  // window.$registry = registry;
-}
 
 Vue.config.productionTip = false
 

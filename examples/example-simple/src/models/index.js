6e839765b0647464d9900d8d21dfdb13fc92f820
Thomas <tw@goalpilot.com>
2020-01-11 15:59:01 -0500

    :fire: removed old files

--- examples/example-simple/src/models/index.js
+++
@@ -1,8 +0,0 @@
-import * as MyGenericStores from './config.js'
-
-for ( let storeName in MyGenericStores ) {
-  // eslint-disable-next-line import/namespace
-  MyGenericStores[ storeName ].setName( storeName )
-}
-
-export default MyGenericStores;

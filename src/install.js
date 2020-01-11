8b99b53707bc34667c5db83e79bd3422482b54a2
Thomas <tw@goalpilot.com>
2020-01-11 15:59:47 -0500

    :construction: Redesigned and simplified helios plugin options

--- src/install.js
+++ src/install.js
@@ -1,6 +1,9 @@
-export let _Vue
+import api from './api'
+import GenericStore from './store'
 
+export let _Vue
 export let _models = {};
+export let _db = {};
 
 export function install (Vue, options) {
 
@@ -10,32 +13,59 @@ export function install (Vue, options) {
   _Vue = Vue
 
   Object.assign(_models, options.models)
+  Object.assign(_db, options.db)
 
   console.log("[GENS] Installing Generic API plugin");
 
-  // const isDef = v => v !== undefined
-
-  /* Object.defineProperty(Vue.prototype, '$db', {
-    // get () { return this._loaderRoot._loader }
-    get () { return this.$root.$options.db }
-  }) */
-
-  if ( options && options.models ) {
-    Object.defineProperty(Vue.prototype, '$models', {
-      get () { return options.models }
-    })
+  if ( !options ) {
+    throw new Error('heliosRX: Missing configuration. Did you supply config with Vue.use("heliosRx", {...})?')
   }
 
-  if ( options && options.api ) {
-    Object.defineProperty(Vue.prototype, '$api', {
-      get () { return options.api }
-    })
+  if ( !options.db ) {
+    throw new Error('heliosRX: Missing configuration "db".')
   }
 
-  if ( process.env.VUE_APP_PRODUCTION === 'false' && process.browser ) {
+  if ( !options.models ) {
+    throw new Error('heliosRX: Missing configuration "models".')
+  }
+
+  // Configure database
+  let dbConfig = options.db
+  // Checking dbConfig.constructor.name === 'Database' won't work in production
+  if ( dbConfig.app && dbConfig.app.database ) {
+    GenericStore.setDefaultDB( options.db );
+  } else if ( typeof dbConfig === 'object' ) {
+    throw new Error('heliosRX: Multi-DB configuration not implemented yet.')
+  } else {
+    throw new Error('heliosRX: Invalid configuration for db.')
+  }
+
+  // Define $models
+  Object.defineProperty(Vue.prototype, '$models', {
+    get () { return options.models }
+  })
+
+  // Merge user api with helios API
+  let mergedApi = api;
+  if ( options.userApi ) {
+    mergedApi = Object.assign({}, api, options.userApi);
+  }
+
+  // Define $api
+  Object.defineProperty(Vue.prototype, '$api', {
+    get () { return mergedApi }
+  })
+
+  // Expose everything to developer console
+  let isDevEnvironment = process.env.VUE_APP_PRODUCTION === 'false' && process.browser;
+  if ( options.devMode === true ||
+       ( options.devMode === undefined && isDevEnvironment ) ) {
     window.$models = options.models;
-    window.$api = options.api;
+    window.$db = options.db
+    window.$api = mergedApi;
   }
 
-  // TODO: Vue.util.defineReactive(this, '_route', this._loader.history.current)
+  // Initialize registry
+  let registry = api.get_registry();
+  registry.commit('INIT_REGISTRY');
 }

54f848533fab00396dd1b6a4ce17f10095f6f675
Thomas <tw@goalpilot.com>
2020-01-13 17:49:08 -0500

    :wrench: rollup config for module bundling

---
+++ build/configs.js
@@ -0,0 +1,112 @@
+const path = require('path')
+const buble = require('@rollup/plugin-buble')
+const replacePlugin = require('@rollup/plugin-replace')
+const resolvePlugin = require('@rollup/plugin-node-resolve')
+const commonjsPlugin = require('@rollup/plugin-commonjs');
+
+const version = process.env.VERSION || require('../package.json').version
+const banner =
+`/**
+ * heliosRX v${version}
+ * (c) ${new Date().getFullYear()} Thomas Weustenfeld
+ * @license MIT
+ */`
+
+const resolve = _path => path.resolve(__dirname, '../', _path)
+
+const configs = {
+  umdDev: {
+    input:  resolve('src/index.js'),
+    file:   resolve('dist/heliosrx.js'),
+    format: 'umd',
+    env:    'development'
+  },
+  umdProd: {
+    input:  resolve('src/index.js'),
+    file:   resolve('dist/heliosrx.min.js'),
+    format: 'umd',
+    env:    'production'
+  },
+  commonjs: {
+    input:  resolve('src/index.js'),
+    file:   resolve('dist/heliosrx.common.js'),
+    format: 'cjs'
+  },
+  esm: {
+    input:  resolve('src/index.esm.js'),
+    file:   resolve('dist/heliosrx.esm.js'),
+    format: 'es'
+  },
+  'esm-browser-dev': {
+    input:  resolve('src/index.esm.js'),
+    file:   resolve('dist/heliosrx.esm.browser.js'),
+    format: 'es',
+    env:    'development',
+    transpile: false
+  },
+  'esm-browser-prod': {
+    input:  resolve('src/index.esm.js'),
+    file:   resolve('dist/heliosrx.esm.browser.min.js'),
+    format: 'es',
+    env:    'production',
+    transpile: false
+  }
+}
+
+function genConfig (opts) {
+  const config = {
+    input: {
+      input: opts.input,
+      plugins: [
+        replacePlugin({
+          __VERSION__: version
+        }),
+        resolvePlugin(),
+        commonjsPlugin({
+          include: 'node_modules/**', // Default: undefined
+        })
+      ]
+    },
+    output: {
+      banner,
+      file: opts.file,
+      format: opts.format,
+      name: 'heliosRX',
+      globals: {
+        vuex: 'Vuex',
+      },
+    },
+    external: ['vuex']
+  }
+
+  if (opts.env) {
+    config.input.plugins.unshift(replacePlugin({
+      'process.env.NODE_ENV': JSON.stringify(opts.env)
+    }))
+  }
+
+  if (opts.transpile !== false) {
+    config.input.plugins.push(buble({
+      transforms: {
+        modules: false,
+      },
+      objectAssign: 'Object.assign'
+    }));
+  }
+
+  return config
+}
+
+function mapValues (obj, fn) {
+  const res = {}
+  Object.keys(obj).forEach(key => {
+    res[key] = fn(obj[key], key)
+  })
+  return res
+}
+
+module.exports = mapValues(configs, genConfig)
+
+// module.exports = {
+//   'umdDev': genConfig(  )
+// }
---
+++ src/index.esm.js
@@ -0,0 +1,34 @@
+import GenericStore from './store/index.js'
+// import { install, _registry as registry } from './install'
+import { install } from './install'
+import registryModule from './registry/module'
+import { setDefaultDB, setDefaultUser, resetGenericStores } from './helpers.js'
+import { UIDMethod, DeleteMode } from './store/enums.js'
+import moment from './moment/index.js'
+import { getRegistry } from './external-deps'
+
+const version = '__VERSION__';
+
+class heliosRX {
+  static install() {}
+}
+
+heliosRX.install = install
+heliosRX.getRegistry = getRegistry;
+heliosRX.GenericStore = GenericStore;
+heliosRX.version = version;
+
+export default heliosRX;
+
+export {
+  version,
+  getRegistry,
+  registryModule,
+  moment,
+  GenericStore,
+  UIDMethod,
+  DeleteMode,
+  setDefaultDB,
+  setDefaultUser,
+  resetGenericStores
+}

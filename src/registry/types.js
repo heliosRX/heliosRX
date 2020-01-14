660dfd322d51fb358ac5a5fd40e98e0110b58ca3
Thomas <tw@goalpilot.com>
2020-01-13 19:00:31 -0500

    :recycle: refactor mutation names

--- src/registry/types.js
+++ src/registry/types.js
@@ -1,7 +1,14 @@
 // types.js
-export const HELIOSRX_INIT_VALUE      = 'genstore/INIT_VALUE'
-export const HELIOSRX_UNSET_VALUE     = 'genstore/UNSET_VALUE';
-export const HELIOSRX_ARRAY_SET       = 'genstore/ARRAY_SET'
-export const HELIOSRX_ARRAY_ADD       = 'genstore/ARRAY_ADD'
-export const HELIOSRX_ARRAY_REMOVE    = 'genstore/ARRAY_REMOVE'
-export const HELIOSRX_SET             = 'genstore/SET'
+export const HELIOSRX_INIT_VALUE      = 'heliosrx/INIT_VALUE'
+export const HELIOSRX_UNSET_VALUE     = 'heliosrx/UNSET_VALUE'
+export const HELIOSRX_ARRAY_SET       = 'heliosrx/ARRAY_SET'
+export const HELIOSRX_ARRAY_ADD       = 'heliosrx/ARRAY_ADD'
+export const HELIOSRX_ARRAY_REMOVE    = 'heliosrx/ARRAY_REMOVE'
+export const HELIOSRX_SET             = 'heliosrx/SET'
+
+/*
+export const HELIOSRX_INIT_REGISTRY            = 'heliosrx/INIT_REGISTRY'
+export const HELIOSRX_RESET_REGISTRY           = 'heliosrx/RESET_REGISTRY'
+export const HELIOSRX_ADD_ENTRY                = 'heliosrx/ADD_ENTRY'
+export const HELIOSRX_SET_ENTRY_STATUS         = 'heliosrx/SET_ENTRY_STATUS'
+export const HELIOSRX_SET_GLOBAL_READY_STATE   = 'heliosrx/SET_GLOBAL_READY_STATE'

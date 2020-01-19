f92d09e2a86456f3b2ff5f31005b9bdf0bd2ebe4
Thomas <tw@wavefab.com>
2020-01-19 15:34:43 -0500

    :wrench: build setup cli

--- packages/cli/build/release.sh
+++ packages/cli/build/release.sh
@@ -1,23 +1,29 @@
 set -e
-echo "Enter release version: "
+echo "Enter heliosRX CLI release version: "
 read VERSION
 
-read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+read -p "Releasing CLI $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing $VERSION ..."
 
   # run tests
-  npm test 2>/dev/null
+  # npm test 2>/dev/null
 
   # build
   VERSION=$VERSION npm run build
 
+  # check if there is uncommitted changes
+  git diff-index --quiet HEAD -- || (echo "Uncommited changes"; exit)
+
   # commit
-  git add -A
-  git commit -m "[build] $VERSION"
-  npm version $VERSION --message "[release] $VERSION"
+  # git add -A
+  # git commit -m "[build] $VERSION"
+
+  # tag
+  git tag -a "cli-$VERSION" -m "[release-cli] $VERSION"
+  npm version $VERSION --message "[release-cli] $VERSION"
 
   # publish
   git push origin refs/tags/v$VERSION

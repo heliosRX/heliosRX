48f1b94efad9bfd75accf04477d60284ada57cdf
Thomas <tw@goalpilot.com>
2020-01-27 12:28:04 -0500

    :wrench: fixed test CI + coverage

--- tests/prepare-dev.sh
+++ tests/prepare-dev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-npm install vue --prefix ./node_modules_peers
-npm install vuex --prefix ./node_modules_peers
-npm install firebase --prefix ./node_modules_peers
+npm install vue --prefix ./peers
+npm install vuex --prefix ./peers
+npm install firebase --prefix ./peers
